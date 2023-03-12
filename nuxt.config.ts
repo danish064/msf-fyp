671d3b10e7c0b8327a71b555a1a79c1a138828a6
Danish <danish.danish.danish2016@gmail.com>
2023-03-12 14:38:56 +0500

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    "@pinia/nuxt",
+    [
+      "@nuxtjs/tailwindcss",
+      {
+        cssPath: "~/assets/css/tailwind.css",
+      },
+    ],
+  ],
+  runtimeConfig: {
+    public: {
+      apiBase: "https://api.find24.app/api/v1",
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-screen">
+    <!-- header -->
+    <div id="header">
+      <div class="flex justify-center items-center h-full">
+        <h1 class="text-4xl text-white">Header</h1>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="postcss">
+#header {
+  height: 100px;
+  background-color: #000;
+}
+</style>
